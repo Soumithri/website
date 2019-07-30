d4d7573fcec0ee251a91f150119d6b9787eca670
Soumithri Chilakamarri <soumithri93@gmail.com>
2019-07-30 14:29:09 -0700

    removing website apps and creating a skeleton to add vue.js

--- mysite/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Project, Author
-
-# Register your models here.
-admin.site.register(Project)
-admin.site.register(Author)
--- mysite/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-
-urlpatterns = [
-    path("", views.HomePageView.as_view(), name='home'),
-    path("project/<int:pk>", views.ProjectDetailView.as_view(), name='project_detail'),
-    path("underconstruction/", views.UnderConstructionView.as_view(), name='under_construction'),
-]
