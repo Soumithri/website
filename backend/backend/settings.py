4bdf4f50335a6d6039ab537fa5e36253cb015613
Soumithri Chilakamarri <soumithri93@gmail.com>
2019-07-31 23:44:45 -0700

    adding production files- nginx/prod.conf, backend/scripts/start_dev.sh, ndinx/Dockerfile .. updating docker-compose.yml for production...created docker-compose.dev.yml for development... However production settings are not working.. Development setting works fine

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -145,4 +145,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = 'static'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,12 +1,13 @@
 import Vue from 'vue';
-import App from './App.vue';
+import BootstrapVue from 'bootstrap-vue'
 import router from './router';
 import store from './store';
 import './registerServiceWorker';
-import BootstrapVue from 'bootstrap-vue'
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+import App from './App.vue';
 
 Vue.use(BootstrapVue)
 Vue.config.productionTip = false;
