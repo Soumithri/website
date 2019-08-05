3d2d788014b901e7e026153cc133c2d4c7a20e40
Soumithri Chilakamarri <soumithri93@gmail.com>
2019-08-05 21:19:50 +0000

    adding ssl_renew.sh shell script which is attached to a cron job in server which checks for automatic SSL renewal at 23:00

--- ssl_renew.sh
+++ ssl_renew.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-/usr/local/bin/docker-compose -f ~/home/website/docker-compose.yml run certbot renew --dry-run \
-&& /usr/local/bin/docker-compose -f ~/home/website/docker-compose.yml kill -s SIGHUP nginx
+/usr/local/bin/docker-compose -f /home/soumithri/home/website/docker-compose.yml run certbot renew \
+&& /usr/local/bin/docker-compose -f /home/soumithri/home/website/docker-compose.yml kill -s SIGHUP nginx
